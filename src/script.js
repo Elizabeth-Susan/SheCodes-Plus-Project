f9d1207be426caf9cb04fd5b36b54c809dc3c971
Elizabeth-Susan <74855891+Elizabeth-Susan@users.noreply.github.com>
2021-01-14 09:28:58 +0200

    Added JavaScript to the Search and My Location buttons

--- src/script.js
+++ src/script.js
@@ -12,6 +12,33 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
-let apiKey = "a8bb545115365cdae986d0ebd7521ddb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+
+function searchForMyLocation(location) {
+  let apiKey = "a8bb545115365cdae986d0ebd7521ddb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function search(city) {
+  let apiKey = "a8bb545115365cdae986d0ebd7521ddb";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+function displayLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchForMyLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let myLocationButton = document.querySelector("#my-location-button");
+myLocationButton.addEventListener("click", displayLocation);
+
+search("Auckland");
