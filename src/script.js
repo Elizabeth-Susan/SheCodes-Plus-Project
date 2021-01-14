4cdec899b61753ab1be488099cd744e7aa3e60f1
Elizabeth-Susan <74855891+Elizabeth-Susan@users.noreply.github.com>
2021-01-14 12:23:31 +0200

    Updated icon and unit elements

--- src/script.js
+++ src/script.js
@@ -1,16 +1,28 @@
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let feelElement = document.querySelector("#feel");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+
+  celciusTemperature = response.data.main.temp;
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
 
   cityElement.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
   descriptionElement.innerHTML = response.data.weather[0].description;
   feelElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchForMyLocation(location) {
@@ -35,10 +47,35 @@ function displayLocation(event) {
   navigator.geolocation.getCurrentPosition(searchForMyLocation);
 }
 
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelciusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 let myLocationButton = document.querySelector("#my-location-button");
 myLocationButton.addEventListener("click", displayLocation);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("Auckland");
